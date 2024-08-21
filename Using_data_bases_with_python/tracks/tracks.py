bbd1b36f010f69f3fcb2a30b2e16f20fc8c7616b
Isaac1602 <86675044+Isaac1602@users.noreply.github.com>
2024-08-21 14:47:57 -0600

    Learning SQL and how to combine them with python

---
+++ Using_data_bases_with_python/tracks/tracks.py
@@ -0,0 +1,90 @@
+import sqlite3
+
+conn = sqlite3.connect('trackdb.sqlite')
+cur = conn.cursor()
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS Genre;
+
+CREATE TABLE Artist (
+    id  INTEGER PRIMARY KEY,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER PRIMARY KEY,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER PRIMARY KEY,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER,
+    genre_id INTEGER
+);
+''')
+
+handle = open('tracks.csv')
+
+# Another One Bites The Dust,Queen,Greatest Hits,55,100,217103
+#   0                          1      2           3  4   5
+
+for line in handle:
+    line = line.strip();
+    pieces = line.split(',')
+    if len(pieces) < 6 : continue
+
+    name = pieces[0]
+    artist = pieces[1]
+    album = pieces[2]
+    count = pieces[3]
+    rating = pieces[4]
+    length = pieces[5]
+    genre = pieces[6]
+
+    print(name, artist, album, count, rating, length, genre)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+                VALUES (?)''', (genre,) )
+    cur.execute('''SELECT id FROM Genre Where name = ? ''', (genre,))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count, genre_id) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, length, rating, count, genre_id ) )
+
+    conn.commit()
+
+
+cur.execute(''' SELECT Track.title, Artist.name, Album.title, Genre.name 
+                            FROM Track JOIN Genre JOIN Album JOIN Artist 
+                            ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+                            AND Album.artist_id = Artist.id
+                            ORDER BY Artist.name ''')
+tabla = cur.fetchall()
+print('')
+print(tabla)
+
+conn.close()
